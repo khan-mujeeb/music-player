147936c42a42b831f354286bf393631df044ad4c
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-01-28 23:55:28 +0530

    view binding enable

--- app/src/main/java/com/example/mmplayer/MainActivity.kt
+++ app/src/main/java/com/example/mmplayer/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.mmplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mmplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (binding!=null) {
+            binding = null
+        }
     }
 }
