5ad628a688116b6857b9a08a950120c458393ed6
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-01-29 12:45:14 +0530

    player activity ui created

---
+++ app/src/main/java/com/example/mmplayer/ui/PlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mmplayer.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mmplayer.R
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
