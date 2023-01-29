8c76be1c2553ca7db28e53e6cd1a1b427b2a2323
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-01-29 10:43:52 +0530

    view binding enable

--- app/src/main/java/com/example/mmplayer/MainActivity.kt
+++ app/src/main/java/com/example/mmplayer/MainActivity.kt
@@ -2,16 +2,26 @@ package com.example.mmplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.mmplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    var binding: ActivityMainBinding? = null
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
+
+        init()
     }
 
+
+    fun init() {
+        val navControl = this.findNavController(R.id.nav_host_fragment)
+        binding!!.bottomNavigation.setupWithNavController(navControl)
+
+    }
     override fun onDestroy() {
         super.onDestroy()
         if (binding!=null) {
