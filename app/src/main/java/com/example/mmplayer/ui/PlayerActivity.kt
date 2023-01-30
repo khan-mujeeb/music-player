ae63aa10227c1d70f1241bb37ddf796c68d23457
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-01-30 21:14:34 +0530

    play pause and minor bug fixes

--- app/src/main/java/com/example/mmplayer/ui/PlayerActivity.kt
+++ app/src/main/java/com/example/mmplayer/ui/PlayerActivity.kt
@@ -3,7 +3,9 @@ package com.example.mmplayer.ui
 import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.mmplayer.MainActivity.Companion.MusicList
+import com.example.mmplayer.R
 import com.example.mmplayer.databinding.ActivityPlayerBinding
 import com.example.mmplayer.modle.Music
 
@@ -12,6 +14,7 @@ class PlayerActivity : AppCompatActivity() {
     var binding: ActivityPlayerBinding? = null
     private lateinit var musicListPA: ArrayList<Music>
     private var mediaPlayer: MediaPlayer? = null
+    private var isPlaying: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityPlayerBinding.inflate(layoutInflater)
@@ -20,21 +23,63 @@ class PlayerActivity : AppCompatActivity() {
         val index = intent.getIntExtra("index", 0)
         val myClass = intent.getStringExtra("class")
 
+        binding!!.playPause.setOnClickListener {
+            if (isPlaying) {
+                pause()
+            }else {
+                play()
+            }
+        }
+
         when(myClass) {
             "HomeActivity" -> {
                 musicListPA = MusicList
-                if(mediaPlayer == null) {
-                    mediaPlayer = MediaPlayer()
-                }
-
-                mediaPlayer!!.reset()
-                mediaPlayer!!.setDataSource(musicListPA[index].path)
-                mediaPlayer!!.prepare()
-                mediaPlayer!!.start()
+                createMediaPlayer(index)
 
             }
         }
     }
+
+    private fun createMediaPlayer(index: Int) {
+        try {
+            if(mediaPlayer == null) {
+                mediaPlayer = MediaPlayer()
+            }
+
+            setMusicData(index)
+            mediaPlayer!!.reset()
+            mediaPlayer!!.setDataSource(musicListPA[index].path)
+            mediaPlayer!!.prepare()
+            mediaPlayer!!.start()
+            binding!!.playPause.setImageResource(R.drawable.ic_baseline_pause_circle_filled_24)
+        }catch (e: java.lang.Exception) {
+            return
+        }
+    }
+
+    private fun play() {
+        isPlaying = true
+        binding!!.playPause.setImageResource(R.drawable.ic_baseline_pause_circle_filled_24)
+        mediaPlayer!!.start()
+    }
+    private fun pause() {
+        isPlaying = false
+        binding!!.playPause.setImageResource(R.drawable.ic_baseline_play_circle_24)
+        mediaPlayer!!.pause()
+    }
+
+    private fun setMusicData(index: Int) {
+
+        binding!!.title.text = musicListPA[index].title
+        binding!!.artist.text = musicListPA[index].artist
+
+        // album art loading
+        Glide.with(this)
+            .load(musicListPA[index].url)
+            .into(binding!!.albumArt)
+
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
